ae82646b0c1a9b3fe68eab1efe76ec53589d9aeb
Maciej Karabin <maciej.m.karabin@gmail.com>
2023-10-17 13:45:27 +0200

    w03 update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # aisd_wyklad
+
+kopiec jest przykładem drzewa AVL wyważonego
+
+drzewo wyważone - h ~ log2 (n); AVL - |h1-h2|<=1
+
+drzewo ściśle wyważone - |n1-n2|<=1
+
+kopiec zapisany w formie wektora:
+dla pola o indeksie i, jego dzieci mają indeksy 2i+1 oraz 2i+2
--- w03/quickSort.c
+++ w03/quickSort.c
@@ -9,7 +9,7 @@ int main() {
 
 int podziel(double t[], int p, int k) {
     int i, j;
-    for (i = p, j = p; i <= k; i++) {
+    for (i = p + 1, j = p; i <= k; i++) {
         if (t[i] < t[p]) {
             double tmp = t[j + 1];
             t[j] = t[i];
@@ -18,9 +18,31 @@ int podziel(double t[], int p, int k) {
         }
     }
     double tmp = t[p];
-    t[p]=t[j];
-    t[j]=tmp;
+    t[p] = t[j];
+    t[j] = tmp;
     return j;
+}
+
+int xpodziel(double t[], int p, int k) {
+    int i = p + 1;
+    int j = k;
+    while (i < j) {
+        while (i < j && t[i] < t[p])
+            i++;
+        while (i < j && t[j] >= t[p])
+            j--;
+        if (i < j) {
+            double tmp = t[i];
+            t[i] = t[j];
+            t[j] = tmp;
+        }
+    }
+    if (t[i] >= t[p])
+        i--;
+    double tmp = t[i];
+    t[i] = t[p];
+    t[p] = tmp;
+    return i;
 }
 
 void qsort_r(double t[], int p, int k) {
@@ -34,3 +56,22 @@ void qsort_r(double t[], int p, int k) {
 void myqsort(double t[], int n) {
     qsort_r(t, 0, n - 1);
 }
+
+//void qsortbr(double t[], int n) {
+//    stack_t stos;
+//    push(stos, 0);
+//    push(stos, n - 1);
+//    while (not_empty(stos)) {
+//        int k = pop(stos);
+//        int p = pop(stos);
+//        int s = xpodziel(t, p, k);
+//        if (s - p > 1) {
+//            push(stos, p);
+//            push(stos, s - 1);
+//        }
+//        if (k - s > 1) {
+//            push(stos, s + 1);
+//            push(stos, k);
+//        }
+//    }
+//}
